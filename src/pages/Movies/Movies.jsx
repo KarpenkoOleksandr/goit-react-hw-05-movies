f34748086e722bffd60089836dfa25aacfb33496
KarpenkoOleksandr <131707071+KarpenkoOleksandr@users.noreply.github.com>
2023-11-27 18:05:00 +0200

    Movies page done

---
+++ src/pages/Movies/Movies.jsx
@@ -0,0 +1,35 @@
+import { useState } from "react";
+import Loader from "components/Loader/Loader";
+import { EditorList } from "pages/EditorsList/EditorsList";
+import { Form } from "react-router-dom";
+import { fetchSearchByKeyword } from "services/Api";
+
+export const Movies = () => {
+    const [searchFilms, setSearchFilms] = useState([]);
+    const [loading, setLoading] = useState(false);
+    const [noMoviesText, setNoMoviesText] = useState(false);
+
+    const searchMovies = queryMovie => {
+        setLoading(true);
+
+      fetchSearchByKeyword(queryMovie)
+        .then(searchResults => {
+          setSearchFilms(searchResults);
+            setNoMoviesText(searchResults.length === 0);
+        })
+        .catch(error => {
+          console.log(error);
+        })
+        .finally(() => {
+          setLoading(false);
+        });
+    }
+    return (
+        <main>
+            <Form searchMovies={searchMovies} />
+            {loading && <Loader />}
+            {noMoviesText && (<p>There is no movies with this request. Please, try again</p>)}
+            {searchFilms && <EditorList films={searchFilms} />}
+        </main>
+    )
+}

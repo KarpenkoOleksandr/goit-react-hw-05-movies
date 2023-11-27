0db5e21109cbb8c0acbb06c36225d9c3bd265fe6
KarpenkoOleksandr <131707071+KarpenkoOleksandr@users.noreply.github.com>
2023-11-27 16:41:10 +0200

    Layout component done

---
+++ src/components/Layout/Layout.styled.jsx
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+import { NavLink } from "react-router-dom";
+
+export const Container = styled.div`
+    
+`;
+
+export const Header = styled.header`
+    
+`;
+
+export const Link = styled(NavLink)`
+    
+`;

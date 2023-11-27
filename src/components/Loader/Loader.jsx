781acb13eb484d28ea570d3c0d5cb932ae5dc462
KarpenkoOleksandr <131707071+KarpenkoOleksandr@users.noreply.github.com>
2023-11-27 16:08:54 +0200

    Loader component done

---
+++ src/components/Loader/Loader.jsx
@@ -0,0 +1,16 @@
+import { Dna } from "react-loader-spinner";
+import { Container } from "./Loader.styled";
+
+export Loader = () => {
+    return (
+        <Container>
+            <Dna
+                visible={true}
+                height="80"
+                width="80"
+                ariaLabel="dna-loading"
+                 wrapperStyle={{}}
+                wrapperClass="dna-wrapper"/>
+        </Container>
+    )
+}

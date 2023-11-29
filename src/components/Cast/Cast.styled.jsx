ad597715a7707e32761ab55b63b5c61798decc03
KarpenkoOleksandr <131707071+KarpenkoOleksandr@users.noreply.github.com>
2023-11-29 16:58:25 +0200

    Styles ready

--- src/components/Cast/Cast.styled.jsx
+++ src/components/Cast/Cast.styled.jsx
@@ -1,9 +1,15 @@
 import styled from "styled-components";
 
 export const List = styled.ul`
-    
+  list-style-type: none;
+  display: flex;
+  flex-wrap: wrap;
+  gap: 24px;    
 `
 
 export const Text = styled.p`
-    
+  font-weight: 600;
+  font-size: 18px;
+  margin-bottom: 10px; 
+  max-width: 200px;
 `
--- src/components/Form/Form.styled.jsx
+++ src/components/Form/Form.styled.jsx
@@ -1,13 +1,42 @@
 import styled from "styled-components";
 
 export const SearchForm = styled.form`
-    
+  display: flex;
+  align-items: center;   
 `;
 
 export const Input = styled.input`
-    
+  padding: 12px 14px;
+  font: inherit;
+  cursor: pointer;
+  border-radius: 4px;
+  border: 2px solid #191d1e;
+  font-weight: 600;
+  font-size: 20px;
+  margin-right: 10px;
+  border-radius: 24px 0 0 24px;
+  outline: none;   
+    &:hover {
+    color: teal;
+    border: 2px solid teal;
+  }
 `;
 
 export const Button = styled.button`
-    
+  padding: 12px 12px;
+  font: inherit;
+  cursor: pointer;
+  border-radius: 4px;
+  border: 2px solid #191d1e;
+  color: #191d1e;
+  font-weight: 600;
+  font-size: 20px;
+  border-radius: 0 24px 24px 0;
+  background-color: #fff;
+
+  &:hover {
+    color: #fff;
+    border: 2px solid teal;
+    background-color: teal;
+  }   
 `;
